7e6538ecbc76f78b10e2ac35fb5fe001d4a3b877
Vladimir <rakhmanov-v@ya.ru>
2023-09-30 19:18:00 +0400

    Домашнее задание ко 2ому семинару.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,21 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
+// import java.util.ArrayList;
+// import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<psyPatient> patientsList = new ArrayList<>();
-        patientsList.add(new psyPatient(30, "Fedor", "+79033044033", "BAR", "Clean"));
-        patientsList.add(new psyPatient(30, "Gven", "+79033044035", "BAR2", "Clean"));
-        patientsList.add(new psyPatient(31, "Little", "+79033044043", "BAR1", "Clean"));
-        patientsList.add(new psyPatient(36, "Nike", "+79033044023", "Schizo", "Delirium"));
-        System.out.println("===== ====== =====");
-        // Инициализация outpatientList с передачей в качестве аргумента списка patientsList возможна только если в
-        // классах patient и psyPatient созданы дополнительные конструкторы без полей и без аргументов.
-        // Почему так?
-        outpatientList out_patient_list = new outpatientList(patientsList);
-        System.out.println(out_patient_list.getPsyPatient(36));
+//        List<psyPatient> patientsList = new ArrayList<>();
+//        patientsList.add(new psyPatient(30, "Fedor", "+79033044033", "BAR", "Clean"));
+//        patientsList.add(new psyPatient(30, "Gven", "+79033044035", "BAR2", "Clean"));
+//        patientsList.add(new psyPatient(31, "Little", "+79033044043", "BAR1", "Clean"));
+//        patientsList.add(new psyPatient(36, "Nike", "+79033044023", "Schizo", "Delirium"));
+//        System.out.println("===== ====== =====");
+//        // Инициализация outpatientList с передачей в качестве аргумента списка patientsList возможна только если в
+//        // классах patient и psyPatient созданы дополнительные конструкторы без полей и без аргументов.
+//        // Почему так?
+//        outpatientList out_patient_list = new outpatientList(patientsList);
+//        System.out.println(out_patient_list.getPsyPatient(36));
 //        System.out.println("=human=");
 //        Human human = new Human("Shmidt");
 //        System.out.println(human.getName());
@@ -35,8 +35,8 @@ public class Main {
 //        actors.add(3, new Human("Cat"));
 
         Market market = new Market();
-        Human human1 = new Human("Ivan");
-        Human human2 = new Human("Vladimir");
+        Human human1 = new Human("Serge");
+        Human human2 = new Human("Gaston");
         market.acceptToMarket(human1);
         market.acceptToMarket(human2);
         market.update();
